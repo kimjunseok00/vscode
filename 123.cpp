9144e91456167ce8f643a867ab262c5932ad155d
code <code>
2025-06-09 14:23:24 +0900

    https://github.com/kimjunseok00/vscode.git

--- 123.cpp
+++ 123.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 using namespace std;
 
+class NamedObject {
+    static int count;
+    int id;
+public:
+    NamedObject() {
+        id = ++count;
+        cout << "Object #" << id << " created." << endl;
+    }
+
+    ~NamedObject() {
+        cout << "Object #" << id << " destroyed." << endl;
+    }
+};
+
+int NamedObject::count = 0;
+
 int main() {
-    cout << "abc" ;
+    NamedObject a;
+    NamedObject b;
+    {
+        NamedObject c;
+    }
+    NamedObject d;
 }
